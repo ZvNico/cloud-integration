85edee5a825a68ff7e704ac647477a29aebc9c8d
zvnico <nicolas.baconnier@efrei.net>
2024-02-22 11:16:12 +0100

    workflow test

--- note-app/next.config.js
+++ note-app/next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack(config, options) {
-	  // Ajoutez la règle pour les fichiers SVG
-	  config.module.rules.push({
-		test: /\.svg$/,
-		use: ['@svgr/webpack'],
-	  });
-  
-	  // Ajoutez "canvas" aux externals
-	  config.externals.push('canvas');
-  
-	  return config;
-	},
+    basePath: '/note-app',
+    webpack(config) {
+        // Ajoutez la règle pour les fichiers SVG
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
 
-  };
-  
-  module.exports = nextConfig;
-  
+        // Ajoutez "canvas" aux externals
+        config.externals.push('canvas');
+
+        return config;
+    },
+
+};
+
+module.exports = nextConfig;
